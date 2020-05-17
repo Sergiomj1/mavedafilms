ed73d1d92cf88aa5690c0f7265349efc62a231c6
advego98 <adrian.vego98@gmail.com>
2020-05-17 17:36:34 +0200

    login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,77 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html>
 
-        <title>Laravel</title>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width = device-width, initial-scale = 1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>mavedafilms</title>
+    <link rel="stylesheet" type="text/css" href="style/styles/form.css">
+    <link rel="stylesheet" type="text/css" href="style/fonts/fuente.css">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+</head>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+<body>
 
-            .full-height {
-                height: 100vh;
-            }
+    <div class="containtpage">
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+        <header>
+            <div class="logo">MAVEDAFILMS</div>
+        </header>
 
-            .position-ref {
-                position: relative;
-            }
+        <div class="emptyspace"></div>
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+        <div class="login-body">
 
-            .content {
-                text-align: center;
-            }
 
-            .title {
-                font-size: 84px;
-            }
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+            <h1 class="title">Inicio Sesión</h1>
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+            <form method="post" name="login-form" class="login">
+                <input type="text" class="forminput" name="login-name" placeholder="Correo Electronico">
+                <input type="text" class="forminput" name="password-name" placeholder="Contraseña">
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+                <div class="helpsection">
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+                    <input type="checkbox" name="checkbox">
+                    <label>Recuerdame</label>
+
+                    <label class="ayuda"><a href="ayuda">Olvide mi contraseña</a></label>
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+                <button class="login-button">Iniciar sesión</button>
+
+                <div class="registerform">¿No tienes cuenta?<a class="suscription" href="registerpart1">Suscríbete ahora</a></div>
+
+            </form>
+
         </div>
-    </body>
-</html>
+
+        <div class="emptyspace2"></div>
+        <footer>
+            <div class="spacefooter"></div>
+            <div class="icon">MAVEDAFILMS</div>
+            <div class="spacefooter"></div>
+            <div class="enlaces">
+
+                <a href="#">Política de privacidad</a>
+                <a href="#">Política de cookies</a>
+                <a href="#">Ayuda</a>
+                <a href="#">Dispositivos compatibles</a>
+                <a href="#">Administrar preferencias</a>
+                <a href="#">Contrato de suscripción</a>
+                <a href="#">Términos y condiciones</a>
+
+            </div>
+
+            <div class="derechosreservados">
+
+                <p>©Mavedafilms. Todos los derechos reservados</p>
+
+            </div>
+
+        </footer>
+    </div>
+
+
+</body></html>
